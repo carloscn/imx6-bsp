7d5a3c14a7e83fe5c8bf2a2bceb0b87b6b8ecff2
Carlos Wei <carlos.wei.hk@gmail.com>
2024-03-09 15:51:33 +0800

    [imx6] updated bsp
    Signed-off-by: Carlos Wei <carlos.wei.hk@gmail.com>

--- bsp/configs/boot.cmd
+++ bsp/configs/boot.cmd
@@ -1,5 +1,3 @@
-part uuid mmc 0:2 uuid
-setenv bootargs console=ttymxc0,115200 root=PARTUUID=${uuid} rootfstype=ext4 fsck.repair=yes rootwait rw
-fatload mmc 0:2 80800000 zImage
-fatload mmc 0:2 83000000 imx6ull-14x14-evk-emmc.dtb
-bootz 80800000 - 83000000
+setenv bootargs 'console=ttymxc0,115200 root=/dev/mmcblk0p3 rootwait rw'
+setenv bootcmd 'mmc dev 0; fatload mmc 0:2 80800000 zImage; fatload mmc 0:2 83000000 imx6ull-14x14-evk.dtb;bootz 80800000 - 83000000;'
+boot
