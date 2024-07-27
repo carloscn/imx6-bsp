a575c92b6caba751f7e0fd542c2489f83ea304f9
Carlos Wei <carlos.wei.hk@gmail.com>
2024-07-27 19:02:22 +0800

    [imx6ull] secure boot enabled
    Signed-off-by: Carlos Wei <carlos.wei.hk@gmail.com>

--- bsp/configs/boot.cmd
+++ bsp/configs/boot.cmd
@@ -1,3 +1,3 @@
 setenv bootargs 'console=ttymxc0,115200 root=/dev/mmcblk0p3 rootwait rw'
-setenv bootcmd 'mmc dev 0; fatload mmc 0:2 80800000 zImage; fatload mmc 0:2 83000000 imx6ull-14x14-evk.dtb;bootz 80800000 - 83000000;'
+setenv bootcmd 'mmc dev 0; fatload mmc 0:2 ${loadaddr} zImage_signed; fatload mmc 0:2 ${fdt_addr} imx6ull-14x14-evk.dtb;bootz ${loadaddr} - ${fdt_addr};'
 boot
--- secure_boot/sign_image/gen_linux_csf.sh
+++ secure_boot/sign_image/gen_linux_csf.sh
@@ -32,34 +32,51 @@ fi
 
 NEXT_4KB_BOUNDARY=$(( (ZIMAGE_SIZE + 0xFFF) & ~0xFFF ))
 NEXT_4KB_BOUNDARY_HEX=$(printf "0x%X" $NEXT_4KB_BOUNDARY)
+IMAGE_SIZE_HEX=$(printf "0x%X" $ZIMAGE_SIZE)
 
-# Generate genIVT script
+echo "[INFO] original size = ${IMAGE_SIZE_HEX}"
+echo "[INFO] round up to next 4K = ${NEXT_4KB_BOUNDARY_HEX}"
+
+KERNEL_LOAD_ADDR=0x80800000
+KERNEL_JUMP_ADDR=$((KERNEL_LOAD_ADDR + 0x1000))
+SELFPOINTER_ADDR=$((KERNEL_LOAD_ADDR + NEXT_4KB_BOUNDARY_HEX))
+CSFPOINTER_ADDR=$((SELFPOINTER_ADDR + 0x20))
+
+KERNEL_JUMP_ADDR_HEX=$(printf "0x%X" $KERNEL_JUMP_ADDR)
+SELFPOINTER_ADDR_HEX=$(printf "0x%X" $SELFPOINTER_ADDR)
+CSFPOINTER_ADDR_HEX=$(printf "0x%X" $CSFPOINTER_ADDR)
+
+echo "[INFO] >>>    KERNEL_LOAD_ADDR:       $KERNEL_LOAD_ADDR"
+echo "[INFO] >>>    KERNEL_JUMP_ADDR:       $KERNEL_JUMP_ADDR_HEX"
+echo "[INFO] >>>    SELFPOINTER_ADDR:       $SELFPOINTER_ADDR_HEX"
+echo "[INFO] >>>    CSFPOINTER_ADDR:        $CSFPOINTER_ADDR_HEX"
+
 cat <<EOF > $GENIVT
 #! /usr/bin/perl -w
 use strict;
 open(my \$out, '>:raw', 'ivt.bin') or die "Unable to open: \$!";
 print \$out pack("V", 0x412000D1); # IVT Header
-print \$out pack("V", 0x10801000); # Jump Location
+print \$out pack("V", ${KERNEL_LOAD_ADDR}); # Jump Location
 print \$out pack("V", 0x0); # Reserved
 print \$out pack("V", 0x0); # DCD pointer
 print \$out pack("V", 0x0); # Boot Data
-print \$out pack("V", 0x$((NEXT_4KB_BOUNDARY_HEX + 0x10000000))); # Self Pointer
-print \$out pack("V", 0x$((NEXT_4KB_BOUNDARY_HEX + 0x10000020))); # CSF Pointer
+print \$out pack("V", ${SELFPOINTER_ADDR_HEX}); # Self Pointer
+print \$out pack("V", ${CSFPOINTER_ADDR_HEX}); # CSF Pointer
 print \$out pack("V", 0x0); # Reserved
 close(\$out);
 EOF
 
 chmod +x $GENIVT
-./$GENIVT
-
+./$GENIVT && hd $IVT_BIN
 # Check genIVT execution result
 if [ $? -ne 0 ]; then
     echo "[ERR] Failed to execute genIVT"
     exit 1
 fi
 
+
 # Pad zImage and check objcopy execution result
-objcopy -I binary -O binary --pad-to=$NEXT_4KB_BOUNDARY --gap-fill=0x00 $ZIMAGE $ZIMAGE_PAD_BIN
+objcopy -I binary -O binary --pad-to=$NEXT_4KB_BOUNDARY_HEX --gap-fill=0x00 $ZIMAGE $ZIMAGE_PAD_BIN
 if [ $? -ne 0 ]; then
     echo "[ERR] objcopy failed"
     exit 1
@@ -69,39 +86,69 @@ fi
 cat $ZIMAGE_PAD_BIN $IVT_BIN > $ZIMAGE_PAD_IVT_BIN
 
 # Check zImage-pad-ivt.bin file size
-ZIMAGE_PAD_SIZE=$(stat -c%s "$ZIMAGE_PAD_IVT_BIN")
-if [ $? -ne 0 ] || [ "$ZIMAGE_PAD_SIZE" -eq 0 ]; then
+ZIMAGE_PAD_SIZE=$(stat -c%s "$ZIMAGE_PAD_BIN")
+ZIMAGE_PAD_IVT_SIZE=$(stat -c%s "$ZIMAGE_PAD_IVT_BIN")
+if [ $? -ne 0 ] || [ "$ZIMAGE_PAD_IVT_SIZE" -eq 0 ]; then
     echo "[ERR] Failed to get zImage-pad-ivt.bin size or size is 0"
     exit 1
 fi
-hex_size=$(printf '0x%x' "$ZIMAGE_PAD_SIZE")
+hex_size=$(printf '0x%x' "$ZIMAGE_PAD_IVT_SIZE")
+no_ivt_hex_size=$(printf '0x%x' "$ZIMAGE_PAD_SIZE")
+
+echo "        #     ------- +-----------------------------+ <-- *load_address: $KERNEL_LOAD_ADDR"
+echo "        #         ^   |                             |"
+echo "        #         |   |                             |"
+echo "        #         |   |                             |"
+echo "        #         |   |                             |"
+echo "        #         |   |           Image             |"
+echo "        # Signed  |   |                             |"
+echo "        #  Data   |   |                             |"
+echo "        #         |   |                             |"
+echo "        #         |   +-----------------------------+ <-- *image size: $IMAGE_SIZE_HEX"
+echo "        #         |   |   Padding to Image size     |"
+echo "        #         |   |        in header            |  -- *image padded size: $no_ivt_hex_size"
+echo "        #         |   |                             | /"
+echo "        #         |   +-----------------------------+ <-- *ivt:  $SELFPOINTER_ADDR_HEX"
+echo "        #         |   |                             |"
+echo "        #         |   |    Image Vector Table       |  -- *image padded + ivt size: $hex_size"
+echo "        #         v   |                             | /"
+echo "        #     ------- +-----------------------------+ <-- *csf:  $CSFPOINTER_ADDR_HEX"
+echo "        #             |                             |"
+echo "        #             |   Command Sequence          |"
+echo "        #             |   File (CSF)                |"
+echo "        #             |                             |"
+echo "        #             +-----------------------------+"
+echo "        #             |   Padding (optional)        |"
+echo "        #             +-----------------------------+"
 
 # Create u-boot.csf content
 csf_content=$(cat <<EOF
 [Header]
 Version = 4.0
+Security Configuration = Open
 Hash Algorithm = sha256
 Engine Configuration = 0
 Certificate Format = X509
 Signature Format = CMS
+Engine = SW
 
 [Install SRK]
 File = "../certs/SRK_1_2_3_4_table.bin"
 Source index = 0
 
 [Install CSFK]
-File = "../certs/CSF2_1_sha256_2048_65537_v3_usr_crt.pem"
+File = "../certs/CSF1_1_sha256_2048_65537_v3_usr_crt.pem"
 
 [Authenticate CSF]
 
 [Install Key]
 Verification index = 0
 Target index = 2
-File = "../certs/IMG2_1_sha256_2048_65537_v3_usr_crt.pem"
+File = "../certs/IMG1_1_sha256_2048_65537_v3_usr_crt.pem"
 
 [Authenticate Data]
 Verification index = 2
-Blocks = 0x10800000 0x0 ${hex_size} "zImage-pad-ivt.bin"
+Blocks = ${KERNEL_LOAD_ADDR} 0x00000000 ${hex_size} "${ZIMAGE_PAD_IVT_BIN}"
 
 EOF
 )
@@ -110,3 +157,4 @@ EOF
 echo "$csf_content" > "$output_file"
 
 echo "[INFO] Generated $output_file with file size $hex_size"
+echo "[INFO] RUN => hab_auth_img ${KERNEL_LOAD_ADDR} 0xffffff $no_ivt_hex_size in your uboot to verify kernel!"
